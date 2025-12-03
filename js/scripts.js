22f8c11e07be139dc107fc3141981c3c63dd5361
Chock21 <martinoscarmateo21@gmail.com>
2025-12-03 01:45:07 -0300

    Estilos
    Para la Fecha y Formato de los partidos

--- js/scripts.js
+++ js/scripts.js
@@ -49,9 +49,9 @@ fetch('jugadores.json')
                                     <p class="my-2 text-sm font-bold text-lg">Pulpito F.C.</p>
                                 </div>
                                 
-                                <div class="flex flex-col gap-2">
+                                <div class="flex flex-col items-center gap-2">
                                     <p class="text-gray-500 text-sm">${fecha}</p>
-                                    <p class="px-2 py-1 my-1 mx-2 text-base font-bold text-white resultado ${vod}">${resultado}</p>
+                                    <p class="w-[80px] px-2 py-1 text-base font-bold text-white resultado ${vod}">${resultado}</p>
                                     <p class="text-gray-500 text-sm">${formato}</p>
                                 </div>
 
