d0695504d62f3b6aaefed709072b57ba528ce9d5
Chock21 <martinoscarmateo21@gmail.com>
2026-01-12 14:28:39 -0300

    Iframe aspect ratio

--- js/scripts.js
+++ js/scripts.js
@@ -189,11 +189,11 @@ fetch('jugadores.json')
                                 ${p.goleadores && p.goleadores.length > 0 ? p.goleadores.map(g => `<p class="flex items-center gap-1">${g.nombre} ${'<img src="imgs/football.png" alt="football" style="width: 14px; height: 14px;" /> '.repeat(g.goles || 0)} ${g.jmv ? '<strong class="border px-1">JMV</strong>' : ''}</p>`).join('') : '<p>No hay goles registrados</p>'}
                             </div>
                             <div class="flex items-center justify-center w-full md:w-1/3 p-4 bg-white rounded-xl">
-                                ${video ? `<iframe class="h-[100%] w-[100%] rounded-xl" src="${video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` : '<p>No hay video disponible</p>'}
+                                ${video ? `<iframe class="w-[100%] aspect-video rounded-xl" src="${video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>` : '<p>No hay video disponible</p>'}
                             </div>
                         </div>
                     </div>
-                    `;
+                `;
             }).join('');
             partidosDiv.innerHTML = htmlPartidos;
 
